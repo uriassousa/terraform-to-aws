f5adb5af869c4d5d977f86dac49507518e6f9a1f
Urias Sousa <uriassousa@hotmail.com>
2024-07-25 22:53:33 -0300

    Terrafrom with fmt -recursive

--- provider.tf
+++ provider.tf
@@ -1,9 +1,14 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "4.48.0"
     }
+  }
+  backend "s3" {
+    bucket = "comdevops"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
   }
 }
 
