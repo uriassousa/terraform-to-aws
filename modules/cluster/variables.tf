b471b1904a6cb39817838057584a93c8ea8eaebf
Urias Sousa <uriassousa@hotmail.com>
2024-07-29 15:25:13 -0300

    feat: Modules, OutPut, Cluster, Sg-Rule

--- modules/cluster/variables.tf
+++ modules/cluster/variables.tf
@@ -1,8 +1,3 @@
-variable "cidr_block" {
-  type        = string
-  description = "Network CIDR block to be used for the VPC"
-}
-
 variable "project_name" {
   type        = string
   description = "Project name to be used to name the resources (Name tag)"
@@ -12,4 +7,14 @@ variable "tags" {
   type        = map(any)
   description = "Tags to be added to AWS resources"
 
+}
+
+variable "public_subnet_1a" {
+  type        = string
+  description = "Subnet to create EKS cluster AZ 1a"
+}
+
+variable "public_subnet_1b" {
+  type        = string
+  description = "Subnet to create EKS cluster AZ 1b"
 }
--- output.tf
+++ output.tf
@@ -1,3 +1,3 @@
-output "subnet_pub_1a" {
-  value = module.eks_network.subnet_pub_1a
+output "eks_vpc_config" {
+  value = module.eks_cluster.eks_vpc_config
 }
