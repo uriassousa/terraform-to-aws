eaea27ca2650c275f2f885bc309a8649a4bf23cf
Urias Sousa <uriassousa@hotmail.com>
2024-07-29 10:44:43 -0300

    feat: created cluster module and IAM permission

--- modules.tf
+++ modules.tf
@@ -4,3 +4,10 @@ module "eks_network" {
   project_name = var.project_name
   tags         = local.tags
 }
+
+module "eks_cluster" {
+  source       = "./modules/cluster"
+  cidr_block   = var.cidr_block
+  project_name = var.project_name
+  tags         = local.tags
+}
