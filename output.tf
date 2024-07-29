4c252e07589ffa00cbcb19f207b4a921da42105a
Urias Sousa <uriassousa@hotmail.com>
2024-07-29 16:19:38 -0300

    feat: new cluster module

--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
-output "eks_vpc_config" {
-  value = module.eks_cluster.eks_vpc_config
+#output "eks_vpc_config" {
+#  value = module.eks_cluster.eks_vpc_config
+#}
+
+output "oidc" {
+  value = module.eks_cluster.oidc
 }
